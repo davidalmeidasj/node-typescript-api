9b8f751b7a3c86969c53a47c1ee96c09f820458f
David <davidasj21@gmail.com>
2021-01-29 08:15:45 -0300

    refactor(StormGlass): change validation to request generics and change test with mock static method

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,6 +6,9 @@ import * as HTTPUtil from '@src/util/request';
 jest.mock('@src/util/request');
 
 describe('StormGlass client', () => {
+
+    const MockedRequestClass = HTTPUtil.Request as jest.Mocked<typeof HTTPUtil.Request>;
+
     const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>;
     it('should return the normalized forecast from the StormGlass service', async () => {
         const lat = -33.792726;
@@ -56,6 +59,7 @@ describe('StormGlass client', () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
+        MockedRequestClass.isRequestError.mockReturnValue(true);
         mockedRequest.get.mockRejectedValue({
             response: {
                 status: 429,
